8c57f7961579a82da404e2b84c219248670750a1
vundi4773 <vundi4773@gmail.com>
2024-04-13 01:51:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+
+}
+
+function fuzzyMatch(drivers, letters){
+return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name  ===  name);
+
+}
